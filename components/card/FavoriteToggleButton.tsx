ef7d5de2987633a51e2a4a8ee09d4d0462ebd9c3
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-15 11:06:09 +0700

    test

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -14,6 +14,7 @@ async function FavoriteToggleButton() {
     return (
         // <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />
         <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+            ji
             <FaHeart />
         </Button>
     );
