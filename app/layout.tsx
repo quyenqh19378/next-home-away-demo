63e421b0a05b8351639ca6f5fe810cbf16fda6b5
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-15 09:33:16 +0700

    add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
             <html lang='en' suppressHydrationWarning>
                 <body className={inter.className}>
                     <Providers>
+                        <Navbar />
                         <main className='container py-10'>{children}</main>
                     </Providers>
                 </body>
